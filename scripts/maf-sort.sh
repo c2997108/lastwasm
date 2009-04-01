e77922cbfae8b6f50c9dab2f65ef842752000ca2
Martin C. Frith <mcfrith@gmail.com>
2009-04-01 10:32:28 +0000

    Removed unnecessary robustness from maf-sort.sh, to avoid bugs in unix sort(!)

--- scripts/maf-sort.sh
+++ scripts/maf-sort.sh
@@ -8,7 +8,12 @@
 
 # Maybe the sorting should consider the strand of the top sequence?
 
-# XXX alignments with differences in whitespace are considered non-identical
+# XXX Preceding whitespace is considered part of the sequence name.  I
+# want to use sort -b, but it seems to be broken in different ways for
+# different versions of sort!
+
+# XXX Alignments with differences in whitespace are considered
+# non-identical.
 
 # Make "sort" use a standard ordering:
 LC_ALL=C
@@ -28,7 +33,7 @@ cat "$@" | tee $tmpfile | grep '^#'
 grep -v '^#' $tmpfile      |  # remove comment lines
 sed '/^a/y/ /!/'           |  # change spaces to '!'s in 'a' lines
 perl -pe 's/\n/#/ if /\S/' |  # join each alignment into one big line
-sort -k2b,2 -k3,3n -k4,4n  |  # sort the lines
+sort -k2,2 -k3,3n -k4,4n   |  # sort the lines
 uniq $opt                  |  # merge identical alignments (don't use sort -u)
 perl -pe 's/#/\n/g'        |  # undo the line-joining
 sed '/^a/y/!/ /'              # change '!'s back to spaces in 'a' lines
