ab1aaad1d020b92857d12e0d924bbe8754f0a3a9
Martin C. Frith <mcfrith@gmail.com>
2013-02-06 05:20:16 +0000

    Made the mmap error messages more detailed.

--- src/fileMap.cc
+++ src/fileMap.cc
@@ -1,10 +1,11 @@
-// Copyright 2010 Martin C. Frith
+// Copyright 2010, 2013 Martin C. Frith
 
 #include "fileMap.hh"
 
 #include "stringify.hh"
 
-#include <cassert>
+#include <cstring>  // strerror
+#include <cerrno>
 #include <stdexcept>
 
 // File mapping requires non-standard-C++ library functions.  I think
@@ -14,6 +15,10 @@
 #include <fcntl.h>  // open
 #include <unistd.h>  // close
 #include <sys/mman.h>  // mmap, munmap
+
+static void err( const std::string& s ) {
+  throw std::runtime_error( s + ": " + std::strerror(errno) );
+}
 
 // This function tries to force the file-mapping to actually get
 // loaded into memory, by reading it sequentially.  Without this,
@@ -37,11 +42,13 @@ void* openFileMap( const std::string& fileName, std::size_t bytes ){
   if( bytes == 0 ) return 0;
 
   int f = open( fileName.c_str(), O_RDONLY );
-  void* m = mmap( 0, bytes, PROT_READ, MAP_SHARED, f, 0 );
-  int e = close(f);
+  if( f < 0 ) err( "can't open file " + fileName );
 
-  if( m == MAP_FAILED || e < 0 )
-    throw std::runtime_error( "can't map file: " + fileName );
+  void* m = mmap( 0, bytes, PROT_READ, MAP_SHARED, f, 0 );
+  if( m == MAP_FAILED ) err( "can't map file " + fileName );
+
+  int e = close(f);
+  if( e < 0 ) err( "can't close file " + fileName );
 
   primeMemory( m, bytes );
 
@@ -51,8 +58,7 @@ void* openFileMap( const std::string& fileName, std::size_t bytes ){
 void closeFileMap( void* begin, std::size_t bytes ){
   if( bytes == 0 ) return;
   int e = munmap( begin, bytes );
-  if( e < 0 ) throw std::runtime_error( "failed to \"munmap\" " +
-                                        stringify(bytes) + " bytes");
+  if( e < 0 ) err( "failed to \"munmap\" " + stringify(bytes) + " bytes" );
 }
 
 }  // end namespace
