e32600da9abb76a18cd206ac01eddde52e3dca68
Martin C. Frith <mcfrith@gmail.com>
2015-11-17 17:21:11 +0900

    Made lastal faster at writing tab format.

--- src/AlignmentWrite.cc
+++ src/AlignmentWrite.cc
@@ -9,11 +9,56 @@
 #include <algorithm>
 #include <cassert>
 #include <cstdio>  // sprintf
+#include <cstring>
 
 // make C++ tolerable:
 #define CI(type) std::vector<type>::const_iterator
 
 using namespace cbrc;
+
+// This writes a "size_t" integer into a char buffer ending at "end".
+// It writes backwards from the end, because that's easier & faster.
+static char *writeSize(char *end, size_t x) {
+  do {
+    --end;
+    *end = '0' + x % 10;
+    x /= 10;
+  } while (x);
+  return end;
+}
+
+class IntText {  // a text representation of an integer
+public:
+  IntText() {}
+  explicit IntText(size_t x) { set(x); }
+  void set(size_t x) { char *e = b + sizeof b; s = e - writeSize(e, x); }
+  const char *begin() const { return b + sizeof b - s; }
+  size_t size() const { return s; }
+private:
+  char b[31];
+  unsigned char s;
+};
+
+class Writer {  // writes characters to an output buffer
+public:
+  explicit Writer(char *startOfOutput) : p(startOfOutput) {}
+  Writer &operator<<(char c) {
+    *p++ = c;
+    return *this;
+  }
+  Writer &operator<<(const IntText &t) {
+    std::memcpy(p, t.begin(), t.size());
+    p += t.size();
+    return *this;
+  }
+  Writer &operator<<(const std::string &t) {
+    std::memcpy(p, t.c_str(), t.size());
+    p += t.size();
+    return *this;
+  }
+private:
+  char *p;
+};
 
 // write x - y as a signed integer
 static void writeSignedDifference( size_t x, size_t y, std::ostream& os ){
@@ -84,6 +129,7 @@ void Alignment::writeTab( const MultiSequence& seq1, const MultiSequence& seq2,
   size_t alnEnd1 = end1();
   size_t w1 = seq1.whichSequence(alnBeg1);
   size_t seqStart1 = seq1.seqBeg(w1);
+  size_t seqLen1 = seq1.seqLen(w1);
 
   size_t size2 = seq2.finishedSize();
   size_t frameSize2 = isTranslated ? (size2 / 3) : 0;
@@ -91,18 +137,28 @@ void Alignment::writeTab( const MultiSequence& seq1, const MultiSequence& seq2,
   size_t alnEnd2 = aaToDna( end2(), frameSize2 );
   size_t w2 = seq2.whichSequence( strand == '+' ? alnBeg2 : size2 - alnBeg2 );
   size_t seqStart2 = strand == '+' ? seq2.seqBeg(w2) : size2 - seq2.seqEnd(w2);
+  size_t seqLen2 = seq2.seqLen(w2);
 
-  size_t b1 = alnBeg1 - seqStart1;
-  size_t b2 = alnBeg2 - seqStart2;
-  size_t r1 = alnEnd1 - alnBeg1;
-  size_t r2 = alnEnd2 - alnBeg2;
-  size_t s1 = seq1.seqLen(w1);
-  size_t s2 = seq2.seqLen(w2);
+  IntText sc(score);
+  std::string n1 = seq1.seqName(w1);
+  std::string n2 = seq2.seqName(w2);
+  IntText b1(alnBeg1 - seqStart1);
+  IntText b2(alnBeg2 - seqStart2);
+  IntText r1(alnEnd1 - alnBeg1);
+  IntText r2(alnEnd2 - alnBeg2);
+  IntText s1(seqLen1);
+  IntText s2(seqLen2);
 
+  size_t s = sc.size() +
+    n1.size() + b1.size() + r1.size() + 1 + s1.size() +
+    n2.size() + b2.size() + r2.size() + 1 + s2.size() + 11;
+  std::vector<char> v(s);
+  Writer w(&v[0]);
   const char t = '\t';
-  os << score << t;
-  os << seq1.seqName(w1) << t << b1 << t << r1 << t << '+'    << t << s1 << t;
-  os << seq2.seqName(w2) << t << b2 << t << r2 << t << strand << t << s2 << t;
+  w << sc << t;
+  w << n1 << t << b1 << t << r1 << t << '+'    << t << s1 << t;
+  w << n2 << t << b2 << t << r2 << t << strand << t << s2 << t;
+  os.write(&v[0], s);
 
   for( CI(SegmentPair) i = blocks.begin(); i < blocks.end(); ++i ){
     if( i > blocks.begin() ){  // between each pair of aligned blocks:
@@ -122,7 +178,7 @@ void Alignment::writeTab( const MultiSequence& seq1, const MultiSequence& seq2,
 
   double fullScore = extras.fullScore;
   char line[256];
-  char* end = writeTaggedItems( evaluer, s2, score, fullScore, t, line );
+  char* end = writeTaggedItems( evaluer, seqLen2, score, fullScore, t, line );
   os.write( line, end - line );
 }
 
