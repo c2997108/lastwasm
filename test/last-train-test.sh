95caf4fc6e09383a99195dcbfa0e1f583af562bd
Martin C. Frith <mcfrith@gmail.com>
2017-06-20 18:11:52 +0900

    Make it easier to feed stdin/pipe to last-train

--- test/last-train-test.sh
+++ test/last-train-test.sh
@@ -16,7 +16,7 @@ trap 'rm -f $db*' EXIT
 
 {
     lastdb $db ../examples/humanMito.fa
-    try last-train -m1 $db ../examples/mouseMito.fa
+    try "last-train -m1 $db < ../examples/mouseMito.fa"
     try last-train -m1 --revsym $db ../examples/mouseMito.fa
     try last-train -m1 --matsym --gapsym $db ../examples/mouseMito.fa
 } |
