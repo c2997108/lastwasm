f64027d5de8a734833e83cc5e590de78c168e885
Martin C. Frith <mcfrith@gmail.com>
2018-03-05 17:43:20 +0900

    Fix dotplot --strands bug with cut sequences

--- test/last-dotplot-test.sh
+++ test/last-dotplot-test.sh
@@ -19,7 +19,9 @@ plotSum () {
     plotSum 102.maf
     plotSum -v -x600 -c orange -r teal --sort1=2 --border-color=brown 102.maf
     plotSum -v -y30 --max-gap1=0 --pad=0.08 --border-pixels=2 --sort1=3 102.maf
+    plotSum -v --strands1=1 102.maf
     plotSum -v --sort2=3 bs100.maf
     plotSum -v -2'?' --sort2=0 --strands2=1 bs100.maf
 } 2>&1 |
+grep -v font |
 diff -u $(basename $0 .sh).out -
