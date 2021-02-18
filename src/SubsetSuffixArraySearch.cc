32a421a85f0d5b85ad804360da0d0ee617861aa1
Martin C. Frith <mcfrith@gmail.com>
2021-02-18 17:40:06 +0900

    Refactor

--- src/SubsetSuffixArraySearch.cc
+++ src/SubsetSuffixArraySearch.cc
@@ -14,6 +14,180 @@ static size_t offGet(const OffPart *p) {
   return x;
 }
 
+SubsetSuffixArray::indexT
+SubsetSuffixArray::lowerBound( indexT beg, indexT end, const uchar* textBase,
+			       const uchar* subsetMap, uchar subset ) const{
+  while (beg < end) {
+    indexT mid = beg + (end - beg) / 2;
+    if (subsetMap[textBase[suffixArray[mid]]] < subset) {
+      beg = mid + 1;
+    } else {
+      end = mid;
+    }
+  }
+  return beg;
+}
+
+SubsetSuffixArray::indexT
+SubsetSuffixArray::upperBound( indexT beg, indexT end, const uchar* textBase,
+			       const uchar* subsetMap, uchar subset ) const{
+  while (beg < end) {
+    indexT mid = beg + (end - beg) / 2;
+    if (subsetMap[textBase[suffixArray[mid]]] <= subset) {
+      beg = mid + 1;
+    } else {
+      end = mid;
+    }
+  }
+  return end;
+}
+
+void SubsetSuffixArray::equalRange( indexT& beg, indexT& end,
+				    const uchar* textBase,
+				    const uchar* subsetMap,
+				    uchar subset ) const{
+  while (beg < end) {
+    indexT mid = beg + (end - beg) / 2;
+    uchar s = subsetMap[textBase[suffixArray[mid]]];
+    if (s < subset) {
+      beg = mid + 1;
+    } else if (s > subset) {
+      end = mid;
+    } else {
+      beg = lowerBound(beg, mid, textBase, subsetMap, subset);
+      end = upperBound(mid + 1, end, textBase, subsetMap, subset);
+      return;
+    }
+  }
+}
+
+SubsetSuffixArray::indexT
+SubsetSuffixArray::lowerBound2( indexT beg, indexT end,
+				const uchar* queryBeg, const uchar* queryEnd,
+				const uchar* textBase,
+				const CyclicSubsetSeed& seed,
+				const uchar* subsetMap ) const{
+  while (beg < end) {
+    indexT mid = beg + (end - beg) / 2;
+    indexT offset = suffixArray[mid];
+    const uchar *t = textBase + offset;
+    const uchar *q = queryBeg;
+    const uchar *s = subsetMap;
+    for (;;) {  // loop over consecutive letters
+      const uchar *textSubsetMap = seed.originalSubsetMap(s);
+      if (textSubsetMap[*t] < s[*q]) {
+	beg = mid + 1;
+	// the next 3 lines are unnecessary, but make it faster:
+	queryBeg = q;
+	textBase = t - offset;
+	subsetMap = s;
+	break;
+      }
+      ++q;  // next query letter
+      if (q == queryEnd) {  // we found a full match to [queryBeg, queryEnd)
+	end = mid;
+	break;
+      }
+      ++t;  // next text letter
+      s = seed.nextMap(s);  // next mapping from letters to subsets
+    }
+  }
+  return beg;
+}
+
+SubsetSuffixArray::indexT
+SubsetSuffixArray::upperBound2( indexT beg, indexT end,
+				const uchar* queryBeg, const uchar* queryEnd,
+				const uchar* textBase,
+				const CyclicSubsetSeed& seed,
+				const uchar* subsetMap ) const{
+  while (beg < end) {
+    indexT mid = beg + (end - beg) / 2;
+    indexT offset = suffixArray[mid];
+    const uchar *t = textBase + offset;
+    const uchar *q = queryBeg;
+    const uchar *s = subsetMap;
+    for (;;) {  // loop over consecutive letters
+      const uchar *textSubsetMap = seed.originalSubsetMap(s);
+      if (textSubsetMap[*t] > s[*q]) {
+	end = mid;
+	// the next 3 lines are unnecessary, but make it faster:
+	queryBeg = q;
+	textBase = t - offset;
+	subsetMap = s;
+        break;
+      }
+      ++q;  // next query letter
+      if (q == queryEnd) {  // we found a full match to [queryBeg, queryEnd)
+	beg = mid + 1;
+	break;
+      }
+      ++t;  // next text letter
+      s = seed.nextMap(s);  // next mapping from letters to subsets
+    }
+  }
+  return end;
+}
+
+void SubsetSuffixArray::equalRange2( indexT& beg, indexT& end,
+				     const uchar* queryBeg,
+				     const uchar* queryEnd,
+				     const uchar* textBase,
+				     const CyclicSubsetSeed& seed,
+				     const uchar* subsetMap ) const{
+  const uchar *qBeg = queryBeg;
+  const uchar *qEnd = qBeg;
+  const uchar *tBeg = textBase;
+  const uchar *tEnd = tBeg;
+  const uchar *sBeg = subsetMap;
+  const uchar *sEnd = sBeg;
+
+  while (beg < end) {
+    indexT mid = beg + (end - beg) / 2;
+    indexT offset = suffixArray[mid];
+    const uchar *q;
+    const uchar *t;
+    const uchar *s;
+    if (qBeg < qEnd) {
+      q = qBeg;
+      t = tBeg + offset;
+      s = sBeg;
+    } else {
+      q = qEnd;
+      t = tEnd + offset;
+      s = sEnd;
+    }
+    uchar x, y;
+    for (;;) {  // loop over consecutive letters
+      const uchar *textSubsetMap = seed.originalSubsetMap(s);
+      x = textSubsetMap[*t];  // this text letter's subset
+      y = s[*q];  // this query letter's subset
+      if (x != y) break;
+      ++q;  // next query letter
+      if (q == queryEnd) {  // we found a full match to [queryBeg, queryEnd)
+	beg = lowerBound2(beg, mid, qBeg, queryEnd, tBeg, seed, sBeg);
+	end = upperBound2(mid + 1, end, qEnd, queryEnd, tEnd, seed, sEnd);
+	return;
+      }
+      ++t;  // next text letter
+      s = seed.nextMap(s);  // next mapping from letters to subsets
+    }
+    if (x < y) {
+      beg = mid + 1;
+      // the next 3 lines are unnecessary, but make it faster:
+      qBeg = q;
+      tBeg = t - offset;
+      sBeg = s;
+    } else {
+      end = mid;
+      // the next 3 lines are unnecessary, but make it faster:
+      qEnd = q;
+      tEnd = t - offset;
+      sEnd = s;
+    }
+  }
+}
+
 // use past results to speed up long matches?
 // could & probably should return the match depth
 void SubsetSuffixArray::match(const PosPart *&begPtr, const PosPart *&endPtr,
@@ -194,178 +368,4 @@ void SubsetSuffixArray::childRange( indexT& beg, indexT& end,
       if( b + 1 == e ) return;  // unnecessary, but may be faster
     }
   }
-}
-
-void SubsetSuffixArray::equalRange( indexT& beg, indexT& end,
-				    const uchar* textBase,
-				    const uchar* subsetMap,
-				    uchar subset ) const{
-  while( beg < end ){
-    indexT mid = beg + (end - beg) / 2;
-    uchar s = subsetMap[ textBase[ suffixArray[mid] ] ];
-    if( s < subset ){
-      beg = mid + 1;
-    }else if( s > subset ){
-      end = mid;
-    }else{
-      beg = lowerBound( beg, mid, textBase, subsetMap, subset );
-      end = upperBound( mid + 1, end, textBase, subsetMap, subset );
-      return;
-    }
-  }
-}
-
-SubsetSuffixArray::indexT
-SubsetSuffixArray::lowerBound( indexT beg, indexT end, const uchar* textBase,
-			       const uchar* subsetMap, uchar subset ) const{
-  while( beg < end ){
-    indexT mid = beg + (end - beg) / 2;
-    if( subsetMap[ textBase[ suffixArray[mid] ] ] < subset ){
-      beg = mid + 1;
-    }else{
-      end = mid;
-    }
-  }
-  return beg;
-}
-
-SubsetSuffixArray::indexT
-SubsetSuffixArray::upperBound( indexT beg, indexT end, const uchar* textBase,
-			       const uchar* subsetMap, uchar subset ) const{
-  while( beg < end ){
-    indexT mid = beg + (end - beg) / 2;
-    if( subsetMap[ textBase[ suffixArray[mid] ] ] <= subset ){
-      beg = mid + 1;
-    }else{
-      end = mid;
-    }
-  }
-  return end;
-}
-
-void SubsetSuffixArray::equalRange2( indexT& beg, indexT& end,
-				     const uchar* queryBeg,
-				     const uchar* queryEnd,
-				     const uchar* textBase,
-				     const CyclicSubsetSeed& seed,
-				     const uchar* subsetMap ) const{
-  const uchar* qBeg = queryBeg;
-  const uchar* qEnd = qBeg;
-  const uchar* tBeg = textBase;
-  const uchar* tEnd = tBeg;
-  const uchar* sBeg = subsetMap;
-  const uchar* sEnd = sBeg;
-
-  while( beg < end ){
-    indexT mid = beg + (end - beg) / 2;
-    indexT offset = suffixArray[mid];
-    const uchar* q;
-    const uchar* t;
-    const uchar* s;
-    if( qBeg < qEnd ){
-      q = qBeg;
-      t = tBeg + offset;
-      s = sBeg;
-    }else{
-      q = qEnd;
-      t = tEnd + offset;
-      s = sEnd;
-    }
-    uchar x, y;
-    for( ; ; ){  // loop over consecutive letters
-      const uchar *textSubsetMap = seed.originalSubsetMap(s);
-      x = textSubsetMap[ *t ];  // this text letter's subset
-      y = s[ *q ];  // this query letter's subset
-      if( x != y ) break;
-      ++q;  // next query letter
-      if( q == queryEnd ){  // we found a full match to [queryBeg, queryEnd)
-	beg = lowerBound2( beg, mid, qBeg, queryEnd, tBeg, seed, sBeg );
-	end = upperBound2( mid + 1, end, qEnd, queryEnd, tEnd, seed, sEnd );
-	return;
-      }
-      ++t;  // next text letter
-      s = seed.nextMap( s );  // next mapping from letters to subsets
-    }
-    if( x < y ){
-      beg = mid + 1;
-      // the next 3 lines are unnecessary, but make it faster:
-      qBeg = q;
-      tBeg = t - offset;
-      sBeg = s;
-    }else{
-      end = mid;
-      // the next 3 lines are unnecessary, but make it faster:
-      qEnd = q;
-      tEnd = t - offset;
-      sEnd = s;
-    }
-  }
-}
-
-SubsetSuffixArray::indexT
-SubsetSuffixArray::lowerBound2( indexT beg, indexT end,
-				const uchar* queryBeg, const uchar* queryEnd,
-				const uchar* textBase,
-				const CyclicSubsetSeed& seed,
-				const uchar* subsetMap ) const{
-  while( beg < end ){
-    indexT mid = beg + (end - beg) / 2;
-    indexT offset = suffixArray[mid];
-    const uchar* t = textBase + offset;
-    const uchar* q = queryBeg;
-    const uchar* s = subsetMap;
-    for( ; ; ){  // loop over consecutive letters
-      const uchar *textSubsetMap = seed.originalSubsetMap(s);
-      if( textSubsetMap[ *t ] < s[ *q ] ){
-	beg = mid + 1;
-	// the next 3 lines are unnecessary, but make it faster:
-	queryBeg = q;
-	textBase = t - offset;
-	subsetMap = s;
-	break;
-      }
-      ++q;  // next query letter
-      if( q == queryEnd ){  // we found a full match to [queryBeg, queryEnd)
-	end = mid;
-	break;
-      }
-      ++t;  // next text letter
-      s = seed.nextMap( s );  // next mapping from letters to subsets
-    }
-  }
-  return beg;
-}
-
-SubsetSuffixArray::indexT
-SubsetSuffixArray::upperBound2( indexT beg, indexT end,
-				const uchar* queryBeg, const uchar* queryEnd,
-				const uchar* textBase,
-				const CyclicSubsetSeed& seed,
-				const uchar* subsetMap ) const{
-  while( beg < end ){
-    indexT mid = beg + (end - beg) / 2;
-    indexT offset = suffixArray[mid];
-    const uchar* t = textBase + offset;
-    const uchar* q = queryBeg;
-    const uchar* s = subsetMap;
-    for( ; ; ){  // loop over consecutive letters
-      const uchar *textSubsetMap = seed.originalSubsetMap(s);
-      if( textSubsetMap[ *t ] > s[ *q ] ){
-	end = mid;
-	// the next 3 lines are unnecessary, but make it faster:
-	queryBeg = q;
-	textBase = t - offset;
-	subsetMap = s;
-        break;
-      }
-      ++q;  // next query letter
-      if( q == queryEnd ){  // we found a full match to [queryBeg, queryEnd)
-	beg = mid + 1;
-	break;
-      }
-      ++t;  // next text letter
-      s = seed.nextMap( s );  // next mapping from letters to subsets
-    }
-  }
-  return end;
 }
