bf76069249b0a3ffe6b50401228bfe9188af7503
Martin C. Frith <mcfrith@gmail.com>
2021-07-05 10:31:01 +0900

    Fix maf-convert to PSL for protein/codon alignment

--- test/maf-convert-test.sh
+++ test/maf-convert-test.sh
@@ -30,6 +30,7 @@ maf2=bs100.maf
     $r -J1e9 psl 102.maf
     $r psl 90089.maf
     $r -p psl frameshift-old.maf
+    $r psl frameshift-new.maf
     $r -n sam $maf2
     head -n999 $maf1 | $r -r 'ID:1 PL:ILLUMINA SM:x' sam
     $r -d sam $maf1
