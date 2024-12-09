6c90615890a7976f8bca5e521a059a981d323e59
Martin C. Frith <mcfrith@gmail.com>
2024-12-09 13:32:53 +0900

    Update a test

--- test/last-train-test.sh
+++ test/last-train-test.sh
@@ -19,6 +19,8 @@ trap 'rm -f $db*' EXIT
     try "last-train -m1 $db < ../examples/mouseMito.fa"
     try last-train -m1 -C2 --revsym $db ../examples/mouseMito.fa
     try last-train -m1 -k16 --matsym --gapsym $db ../examples/mouseMito.fa
-    try last-train -Q1 $db bs100.fastq
+
+    lastdb -uBISF -S2 $db ../examples/humanMito.fa
+    try last-train -S0 -Q1 $db bs100.fastq
 } 2>&1 |
     grep -v '^# lastal' | diff -u last-train-test.out -
